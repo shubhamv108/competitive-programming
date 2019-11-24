1c9da645bc53bda57a56ba12b1d080831678756b
Shubham <shubhamvarshney@outlook.com>
2019-11-24 23:58:05 +0530

    Added code for resizing hashmap table

--- src/main/java/code/collections/HashMap.java
+++ src/main/java/code/collections/HashMap.java
@@ -8,7 +8,7 @@ public class HashMap<K, V> {
     int bucketCount = 16;
     float bucketCountThreshold = 0.75f;
     int treefyThresholdForBucketItems = 8;
-    NodeMap<K, V>[] arr = new NodeMap[bucketCount];
+    Node<K, V>[] arr = new Node[bucketCount];
 
     HashMap() {}
     HashMap(float bucketCountThreshold) {
@@ -32,20 +32,20 @@ public class HashMap<K, V> {
         }
         int hashCode = k.hashCode();
         int bucketNo = hashCode % bucketCount;
-        NodeMap<K, V> bucketItem = arr[bucketNo];
+        Node<K, V> bucketItem = arr[bucketNo];
         if (bucketItem == null) {
-            arr[bucketNo] = new MapNode<>(k, v);
+            arr[bucketNo] = new MapNode<>(k, v, hashCode);
             size++;
         } else {
-            if (bucketItem instanceof TreeMapNode) {
-                ((RBTreeMap) bucketItem).put(k, v);
+            if (bucketItem instanceof RBTreeMap) {
+                ((RBTreeMap) bucketItem).put(k, v, hashCode);
                 size++;
             } else {
                 int bucketItemCount = 0;
                 MapNode bucketListItem = (MapNode) bucketItem;
                 while (bucketListItem != null) {
                     if (++bucketItemCount == treefyThresholdForBucketItems) {
-                        arr[bucketNo] = treefyAndInsert(bucketNo, k, v);
+                        arr[bucketNo] = treefyAndInsert(bucketNo, k, v, hashCode);
                         size++;
                         break;
                     }
@@ -54,7 +54,7 @@ public class HashMap<K, V> {
                         break;
                     }
                     if (bucketListItem.next == null) {
-                        bucketListItem.next = new MapNode<>(k, v);
+                        bucketListItem.next = new MapNode<>(k, v, hashCode);
                         size++;
                         break;
                     }
@@ -64,41 +64,98 @@ public class HashMap<K, V> {
         }
     }
 
-    void increaseBucketsAndReIndex() {
-        NodeMap[] arrCopy = new NodeMap[bucketCount * 2];
-    }
-
-    /** ToDo */
-    void increaseBuckets() {
-        NodeMap[] arrCopy = new NodeMap[bucketCount * 2];
-        for (int i = 0; i < bucketCount; i++) {
-            arrCopy[i] = arr[i];
-        }
-        arr = arrCopy;
-        bucketCount *= 2;
-    }
-
-    NodeMap<K, V> treefyAndInsert(int bucketNo, K k, V v) {
-        RBTreeMap<K, V> tree = treefy(bucketNo);
-        tree.put(k, v);
+    Node<K, V> treefyAndInsert(int bucketNo, K k, V v, int hashCode) {
+        RBTreeMap<K, V> tree = treefy(bucketNo, hashCode);
+        tree.put(k, v, hashCode);
         return tree;
     }
 
-    RBTreeMap<K, V> treefy(int bucketNo) {
-        NodeMap<K, V> bucketItem = arr[bucketNo];
+    RBTreeMap<K, V> treefy(int bucketNo, int hashCode) {
+        Node<K, V> bucketItem = arr[bucketNo];
         RBTreeMap<K, V> rbTreeMap = new RBTreeMap<>();
         MapNode<K, V> bucketListItem = (MapNode) bucketItem;
         while (bucketListItem != null) {
-            rbTreeMap.put(bucketListItem.k, bucketListItem.v);
+            rbTreeMap.put(bucketListItem.k, bucketListItem.v, hashCode);
             bucketListItem = bucketListItem.next;
         }
         return rbTreeMap;
     }
 
+    /** ToDo */
+    void increaseBuckets() {
+        Node[] arrCopy = new Node[bucketCount * 2];
+        for (int currentBucketLocation = 0; currentBucketLocation < bucketCount; currentBucketLocation++) {
+            Node<K, V> currentBucket = arr[currentBucketLocation];
+            if (currentBucket instanceof RBTreeMap) {
+                /** ToDo */
+//                performSplitOfTree((RBTreeMap<K, V>) currentBucket, currentBucketLocation, arrCopy);
+            } else {
+                MapNode<K, V> currentNode = (MapNode<K, V>) currentBucket;
+                if (currentNode.next == null) {
+                    arrCopy[currentNode.hash & bucketCount - 1] = currentNode;
+                } else {
+                    MapNode<K, V> currentBucketLocation__LocationBucketList__Tail = null;
+                    MapNode<K, V> currentBucketLocation__LocationBucketList__Head = null;
+                    MapNode<K, V> currentBucketLocation_plus_CurrentBucketCount__LocationBucketList__Tail = null;
+                    MapNode<K, V> currentBucketLocation_plus_CurrentBucketCount__LocationBucketList__Head = null;
+                    while (currentNode != null) {
+                        if ((currentNode.hash & bucketCount) == 0) {
+                            if (currentBucketLocation__LocationBucketList__Head == null) {
+                                currentBucketLocation__LocationBucketList__Head = currentBucketLocation__LocationBucketList__Tail = currentNode;
+                                arrCopy[currentBucketLocation] = currentBucketLocation__LocationBucketList__Head;
+                            } else {
+                                currentBucketLocation__LocationBucketList__Tail.next = currentNode;
+                            }
+                        } else {
+                            if (currentBucketLocation__LocationBucketList__Head == null) {
+                                currentBucketLocation_plus_CurrentBucketCount__LocationBucketList__Head = currentBucketLocation__LocationBucketList__Tail = currentNode;
+                                arrCopy[currentBucketLocation + bucketCount] = currentBucketLocation_plus_CurrentBucketCount__LocationBucketList__Head;
+                            } else {
+                                currentBucketLocation_plus_CurrentBucketCount__LocationBucketList__Tail.next = currentNode;
+                            }
+                        }
+                        currentNode = currentNode.next;
+                    }
+                }
+            }
+        }
+        arr = arrCopy;
+        bucketCount *= 2;
+    }
+
+    /** ToDo */
+//    void performSplitOfTree(RBTreeMap<K,V> currentBucketTree, int currentBucketLocation, MapNode<K, V>[] arrCopy) {
+//        MapNode<K, V> currentBucketLocation__LocationBucketList__Tail = null;
+//        MapNode<K, V> currentBucketLocation__LocationBucketList__Head = null;
+//        MapNode<K, V> currentBucketLocation_plus_CurrentBucketCount__LocationBucketList__Tail = null;
+//        MapNode<K, V> currentBucketLocation_plus_CurrentBucketCount__LocationBucketList__Head = null;
+//        currentBucketTree.get().entrySet().forEach(e -> e.hash());
+//        while (iterator.hasNext()) {
+//            if ((currentNode.hash & bucketCount) == 0) {
+//                if (currentBucketLocation__LocationBucketList__Head == null) {
+//                    currentBucketLocation__LocationBucketList__Head = currentBucketLocation__LocationBucketList__Tail = currentNode;
+//                    arrCopy[currentBucketLocation] = currentBucketLocation__LocationBucketList__Head;
+//                } else {
+//                    currentBucketLocation__LocationBucketList__Tail.next = currentNode;
+//                }
+//            } else {
+//                if (currentBucketLocation__LocationBucketList__Head == null) {
+//                    currentBucketLocation_plus_CurrentBucketCount__LocationBucketList__Head = currentBucketLocation__LocationBucketList__Tail = currentNode;
+//                    arrCopy[currentBucketLocation + bucketCount] = currentBucketLocation_plus_CurrentBucketCount__LocationBucketList__Head;
+//                } else {
+//                    currentBucketLocation_plus_CurrentBucketCount__LocationBucketList__Tail.next = currentNode;
+//                }
+//            }
+//            currentNode = currentNode.next;
+//        }
+//
+//    }
+
+
     V get(K k) {
         int hashCode = k.hashCode();
         int bucketNo = hashCode % bucketCount;
-        NodeMap<K, V> bucketItem = arr[bucketNo];
+        Node<K, V> bucketItem = arr[bucketNo];
         if (bucketItem instanceof RBTreeMap) {
             return (V) ((RBTreeMap) bucketItem).get(k);
         } else {
@@ -127,34 +184,45 @@ public class HashMap<K, V> {
 }
 
 
-interface NodeMap<K, V> {}
+interface Node<K, V> {}
 
-class MapNode<K, V> implements NodeMap<K, V> {
+class MapNode<K, V> implements Node<K, V> {
     K k;
     V v;
+    int hash;
     MapNode<K, V> next;
-    MapNode(K k, V v) {
+    MapNode(K k, V v, int hash) {
         this.k = k;
         this.v = v;
+        this.hash = hash;
+    }
+
+    public K getKey() {
+        return this.k;
+    }
+
+
+    public V getValue() {
+        return this.v;
+    }
+
+
+    public V setValue(V value) {
+        V oldValue = this.v;
+        this.v = value;
+        return oldValue;
     }
 }
 
-class TreeMapNode<K, V> extends MapNode<K, V> {
-    TreeMapNode<K, V> left;
-    TreeMapNode<K, V> right;
-    TreeMapNode(K k, V v) {
-        super(k, v);
-    }
-}
-
-class RBTreeMap<K, V> implements NodeMap {
+/** ToDo */
+class RBTreeMap<K, V> implements Node<K, V> {
 
     TreeMapNode<K, V> root;
 
     /**
-     * Replace java.util.TreeMap with own implementation of RBTree
+     * Replace java.util.TreeMap with implementation of RBTree
      */
-    TreeMap<K, V> tree = new TreeMap<>();
+    java.util.TreeMap<K, V> tree = new java.util.TreeMap<>();
 
     /**
      * ToDo
@@ -162,8 +230,8 @@ class RBTreeMap<K, V> implements NodeMap {
      * @param k
      * @param v
      */
-    void put(K k, V v) {
-        TreeMapNode<K, V> n = new TreeMapNode<>(k, v);
+    void put(K k, V v, int hashCode) {
+        TreeMapNode<K, V> n = new TreeMapNode<>(k, v, hashCode);
 
         tree.put(k, v);
 
@@ -172,4 +240,22 @@ class RBTreeMap<K, V> implements NodeMap {
     V get(K k) {
         return tree.get(k);
     }
+
+    int size() {
+        return tree.size();
+    }
+
+
+    class TreeMapNode<K, V> extends MapNode<K, V> implements java.util.Map.Entry<K, V> {
+        TreeMapNode<K, V> left;
+        TreeMapNode<K, V> right;
+        TreeMapNode(K k, V v, int hash) {
+            super(k, v, hash);
+        }
+    }
+
+    TreeMap get() {
+        return tree;
+    }
+
 }
