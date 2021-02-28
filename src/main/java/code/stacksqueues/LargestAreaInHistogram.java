11e39b84450f8b4d1c89f894acbd8f62e790e771
Shubham <16763337+shubham-v@users.noreply.github.com>
2021-03-01 02:32:48 +0530

    af

--- src/main/java/code/stacksqueues/LargestAreaInHistogram.java
+++ src/main/java/code/stacksqueues/LargestAreaInHistogram.java
@@ -1,4 +1,39 @@
 package code.stacksqueues;
 
+import java.util.Stack;
+
 public class LargestAreaInHistogram {
+
+    public int largestRectangleArea(int[] heights) {
+        int maxArea = 0;
+
+        Stack<Integer> s = new Stack<>();
+
+        for (int i = 0; i < heights.length; i++) {
+            while (!s.empty() && heights[i] < heights[s.peek()]) {
+                int top = s.pop();
+                if (s.isEmpty()) {
+                    maxArea = Math.max(maxArea, heights[top] * i);
+                } else {
+                    maxArea = Math.max(maxArea, heights[top] * (i - s.peek() - 1));
+                }
+            }
+            s.push(i);
+        }
+
+        if (!s.isEmpty()) {
+            int i = heights.length;
+            while (!s.empty()) {
+                int top = s.pop();
+                if (s.isEmpty()) {
+                    maxArea = Math.max(maxArea, heights[top] * i);
+                } else {
+                    maxArea = Math.max(maxArea, heights[top] * (i - s.peek() - 1));
+                }
+            }
+        }
+
+        return maxArea;
+    }
+
 }
