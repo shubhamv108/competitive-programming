de2bac2163de3ec87194170111b75cabb0ef1b08
Shubham <16763337+shubham-v@users.noreply.github.com>
2022-11-13 13:33:06 +0530

    uf

--- src/main/java/code/api/query/clauses/where/Condition.java
+++ src/main/java/code/api/query/clauses/where/Condition.java
@@ -1,9 +1,8 @@
 package code.api.query.clauses.where;
 
-import code.api.Address;
 import code.api.Data;
+import code.api.ReflectionUtils;
 
-import java.lang.reflect.Field;
 import java.util.Arrays;
 
 public class Condition {
@@ -12,7 +11,7 @@ public class Condition {
     String[] values;
 
     boolean apply(Data data) throws IllegalAccessException {
-        Object dataValue = this.getValueForField(fieldPath, 0, data);
+        Object dataValue = ReflectionUtils.getValueForField(fieldPath, 0, data);
         if (Operation.EQUALS.equals(operation)) {
             return values[0].equals(dataValue);
         }
@@ -20,25 +19,6 @@ public class Condition {
             return Arrays.stream(values).anyMatch(value -> value.equals(dataValue));
         }
         return false;
-    }
-
-    private static Object getValueForField(String[] fieldPath, int index, Object object)
-            throws IllegalAccessException {
-        for (Field field : object.getClass().getDeclaredFields()) {
-            if (field.getName().equals(fieldPath[index])) {
-                field.setAccessible(true);
-                if (index == fieldPath.length - 1)
-                    return field.get(object);
-                return getValueForField(fieldPath, index + 1, field.get(object));
-            }
-        }
-        return "";
-    }
-
-    public static void main(String[] args) throws IllegalAccessException {
-        Data data = new Data(new Address("test"));
-        System.out.println(
-                getValueForField(new String[] {"address", "city"}, 0, data));
     }
 
     @Override
