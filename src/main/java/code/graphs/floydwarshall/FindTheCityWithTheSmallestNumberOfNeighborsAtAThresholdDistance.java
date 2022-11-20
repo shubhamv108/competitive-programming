14dc12c5e9fed4d37ebbeda65967f3454cee90ee
Shubham <16763337+shubham-v@users.noreply.github.com>
2022-11-20 06:28:36 +0530

    uf

---
+++ src/main/java/code/graphs/floydwarshall/FindTheCityWithTheSmallestNumberOfNeighborsAtAThresholdDistance.java
@@ -0,0 +1,41 @@
+package code.graphs.floydwarshall;
+
+public class FindTheCityWithTheSmallestNumberOfNeighborsAtAThresholdDistance {
+    class Solution {
+        public int findTheCity(int n, int[][] edges, int distanceThreshold) {
+            Integer[][] distance = new Integer[n][n];
+            for (int[] e: edges)
+                distance[e[0]][e[1]] = distance[e[1]][e[0]] = e[2];
+
+            for (int  i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    if (i == j)
+                        distance[i][i] = 0;
+                    else if (distance[i][j] == null)
+                        distance[i][j] = 10001;
+                }
+            }
+
+            for (int k = 0; k < n; k++)
+                for (int i = 0; i < n; i++)
+                    for (int j = 0; j < n; j++)
+                        distance[i][j] = Math.min(
+                                distance[i][j],
+                                distance[i][k] + distance[k][j]);
+
+            int min = n, result = 0;
+            for (int i = 0; i < n; i++) {
+                int count = 0;
+                for (int j = 0; j < n; j++)
+                    if (distance[i][j] <= distanceThreshold)
+                        count++;
+
+                if (count <= min) {
+                    min = count;
+                    result = i;
+                }
+            }
+            return result;
+        }
+    }
+}
---
+++ src/main/java/code/unionfind/MostStonesRemovedWithSameRowOrSameColumn.java
@@ -0,0 +1,91 @@
+package code.unionfind;
+
+import java.util.HashMap;
+import java.util.HashSet;
+
+public class MostStonesRemovedWithSameRowOrSameColumn {
+    class Solution {
+        public int removeStones(int[][] A) {
+            int combinedStones = 0;
+            HashSet<int[]> visited = new HashSet<>();
+            for (int[] a : A) {
+                if (visit(a, A, visited))
+                    combinedStones++;
+            }
+            return A.length - combinedStones;
+        }
+
+        boolean visit(int[] x, int[][] A, HashSet<int[]> visited) {
+            if (visited.contains(x))
+                return false;
+
+            visited.add(x);
+            for (int[] a : A)
+                if (a[0] == x[0] || a[1] == x[1])
+                    visit(a, A, visited);
+
+            return true;
+        }
+    }
+
+    class Solution2 {
+        HashMap<Integer, Integer> f = new HashMap<>();
+        int islands = 0;
+
+        public int removeStones(int[][] stones) {
+            for (int i = 0; i < stones.length; ++i)
+                union(stones[i][0], ~stones[i][1]);
+            return stones.length - islands;
+        }
+
+        public int find(int x) {
+            if (f.putIfAbsent(x, x) == null)
+                islands++;
+            if (x != f.get(x))
+                f.put(x, find(f.get(x)));
+            return f.get(x);
+        }
+
+        public void union(int x, int y) {
+            x = find(x);
+            y = find(y);
+            if (x != y) {
+                f.put(x, y);
+                islands--;
+            }
+
+        }
+    }
+
+    class Solution3 {
+        int[] parent = new int[20001];
+        int islands = 0;
+
+        public int removeStones(int[][] stones) {
+            for (int i = 0; i < stones.length; ++i) {
+                union(stones[i][0], stones[i][1] + 10000);
+            }
+
+            return stones.length - islands;
+        }
+
+        public int find(int x) {
+            if (parent[x] == 0) {
+                islands++;
+                parent[x] = x;
+            }
+            if (x != parent[x])
+                parent[x] = find(parent[x]);
+            return parent[x];
+        }
+
+        public void union(int x, int y) {
+            x = find(x);
+            y = find(y);
+            if (x != y) {
+                parent[x] = y;
+                islands--;
+            }
+        }
+    }
+}
