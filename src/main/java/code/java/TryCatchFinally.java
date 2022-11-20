3574f93eef73ac0c7139a8820d819d394cd31823
Shubham <16763337+shubham-v@users.noreply.github.com>
2022-11-20 14:50:14 +0530

    af

---
+++ src/main/java/code/java/TryCatchFinally.java
@@ -0,0 +1,31 @@
+package code.java;
+
+public class TryCatchFinally {
+
+    class Solution {
+        int solve() {
+            int returnValue = 10;
+
+            try {
+                int[] A = {};
+                System.out.println(A[2]);
+            } catch (Exception exception) {
+                System.out.println("Catch Block: " + returnValue);
+                return returnValue + 100;
+            } finally {
+                returnValue += 10;
+                System.out.println("Finally Block: " + returnValue);
+                return returnValue;
+            }
+
+//            return returnValue;
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(
+                new TryCatchFinally().new Solution().solve()
+        );
+    }
+
+}
---
+++ src/main/java/code/strings/MaxLengthSubstringWithHighestFrequency.java
@@ -0,0 +1,51 @@
+package code.strings;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MaxLengthSubstringWithHighestFrequency {
+    class Solution {
+        String solve(String A) {
+            return solve(A.toCharArray());
+        }
+
+        String solve(char[] A) {
+
+            int len = A.length;
+
+            Map<String, Integer> freq = new HashMap<>();
+
+            for (int i = 0; i < len; i++) {
+                String s = "";
+                for (int j = i; j < len; j++) {
+                    s += A[j];
+                    freq.put(s, freq.getOrDefault(s, 0) + 1);
+                }
+            }
+
+            int maxFreq = 0;
+
+            String result = "";
+            for (Map.Entry<String, Integer> entry : freq.entrySet()) {
+                if (entry.getValue() > maxFreq) {
+                    maxFreq = entry.getValue();
+                    result = entry.getKey();
+                } else if (entry.getValue() == maxFreq) {
+                    String ss = entry.getKey();
+                    if (ss.length() > result.length())
+                        result = ss;
+                }
+            }
+
+            return result;
+        }
+    }
+
+    public static void main(String[] args) {
+        MaxLengthSubstringWithHighestFrequency obj = new MaxLengthSubstringWithHighestFrequency();
+        Solution solution = obj.new Solution();
+        System.out.println(
+                solution.solve("abcabc")
+        );
+    }
+}
