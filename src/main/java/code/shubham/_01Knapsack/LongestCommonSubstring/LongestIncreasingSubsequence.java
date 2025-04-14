64f1bd649194ca43e82e83a8c3dc0797f0935170
Shubham Varshney <shubhamvarshney@outlook.com>
2025-04-15 03:23:37 +0530

    af

--- src/main/java/code/shubham/_01Knapsack/LongestCommonSubstring/LongestIncreasingSubsequence.java
+++ src/main/java/code/shubham/_01Knapsack/LongestCommonSubstring/LongestIncreasingSubsequence.java
@@ -16,4 +16,33 @@ public class LongestIncreasingSubsequence {
             return result;
         }
     }
+
+    // O(n * log n)
+    class Solution2 {
+        public int lengthOfLIS(int[] A) {
+            int result = 0;
+
+            int[] dp = new int[A.length];
+            for (int a : A) {
+                int l = 0, r = result;
+                while (l != r) {
+                    int m = l + (r - l) / 2;
+                    if (dp[m] < a)
+                        l = m + 1;
+                    else
+                        r = m;
+                }
+
+                dp[l] = a;
+                if (l == result)
+                    ++result;
+            }
+
+            return result;
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new LongestIncreasingSubsequence().new Solution2().lengthOfLIS(new int[] { 10,9,2,5,3,7,101,18 }));
+    }
 }
